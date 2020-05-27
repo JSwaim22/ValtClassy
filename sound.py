0bba171af0bc6bbcbbb07cf0aa85e446b305db60
JSwaim22 <52038890+JSwaim22@users.noreply.github.com>
2020-05-27 15:16:38 -0400

    Update sound.py

--- sound.py
+++ sound.py
@@ -2,7 +2,7 @@ from periphery import GPIO
 import simpleaudio as sa
 
 def main():
-    wave_obj = sa.WaveObject.from_wave_file("~/ValtWAV/welcome.wav")
+    wave_obj = sa.WaveObject.from_wave_file("welcome.wav")
     play_obj = wave_obj.play()
     play_obj.wait_done()
 
