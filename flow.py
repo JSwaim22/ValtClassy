f4dd97e5e73a0fcf3909a462887087e209a2d7e5
JSwaim22 <52038890+JSwaim22@users.noreply.github.com>
2020-05-28 16:26:19 -0400

    Update flow.py

--- flow.py
+++ flow.py
@@ -1,29 +1,3 @@
-# Copyright 2019 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-"""A demo which runs object classification on camera frames.
-Run default object detection:
-python3 classify.py
-Choose different camera and input encoding
-python3 classify.py --videosrc /dev/video1 --videofmt jpeg
-"""
-
-"""Runs a model on the edgetpu.
-Useage:
-python3 run_model.py --model_file model_edgetpu.tflite
-"""
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
